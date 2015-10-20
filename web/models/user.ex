b3fbac5749c77440d9ed2f5a1668b14f23931e77
Michael Ries <michael@riesd.com>
2015-10-19 21:22:19 -0600

    minor updates to view and changeset

--- web/models/user.ex
+++ web/models/user.ex
@@ -15,6 +15,9 @@ defmodule Rumbl.User do
     model
     |> cast(params, ~w(name username email), [])
     |> update_change(:email, &String.downcase/1)
+    |> update_change(:email, &String.strip/1)
+    |> update_change(:username, &String.downcase/1)
+    |> update_change(:username, &String.strip/1)
     |> validate_length(:username, min: 1, max: 20)
   end
 end
